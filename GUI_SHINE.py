04c5f88500c60ffabbbfc8a0464075dc469cd6eb
Davide <d.tornotti@campus.unimib.it>
2024-12-10 16:59:44 +0100

    Minor tweaks

--- GUI_SHINE.py
+++ GUI_SHINE.py
@@ -12,11 +12,11 @@ class GUI_SHINE:
     def __init__(self, root):
         """Initialize the GUI."""
         self.root = root
-        self.root.title("SHINE")
+        self.root.title("SHINE (v0 version)")
         
         # Set minimum size for the window
-        self.root.geometry("800x800")  # Width x Height
-        self.root.minsize(600, 400)
+        self.root.geometry("1200x800")  # Width x Height
+        self.root.minsize(600, 600)
 
         # Configure grid to make it expandable
         self.root.grid_rowconfigure(0, weight=1)
@@ -32,32 +32,18 @@ class GUI_SHINE:
         self.root.iconphoto(False, logo)  
 
         
-        # Add a scrollbar to handle limited window size
-        #self.canvas = tk.Canvas(root)
-        #self.canvas.grid_rowconfigure(0, weight=1)
-        #self.canvas.grid_columnconfigure(0, weight=1)
-        #self.scrollbar = tk.Scrollbar(root, orient="vertical", command=self.canvas.yview)
-        #self.scrollable_frame = tk.Frame(self.canvas)
-        
-        #self.scrollable_frame.bind("<Configure>", lambda e: self.canvas.configure(scrollregion=self.canvas.bbox("all")))
-        
-        #self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
-        #self.canvas.configure(yscrollcommand=self.scrollbar.set)
-        
-        #self.canvas.grid(row=0, column=0, sticky="nsew")
-        #self.scrollbar.grid(row=0, column=1, sticky="ns")
 
         # === Input Arguments Section (grpinp) ===
         self.frame_input = tk.LabelFrame(root, text="Input Arguments", padx=10, pady=10)
         self.frame_input.grid(row=0, column=0, padx=10, pady=5, sticky="ew")
         
         # Cube and Variance Cube (Browse)
-        self.entry_cube = self.create_input_row_with_browse(self.frame_input, "Cube (Required):", 0)
+        self.entry_cube    = self.create_input_row_with_browse(self.frame_input, "Cube (Required):", 0)
         self.entry_varcube = self.create_input_row_with_browse(self.frame_input, "Variance Cube (Required):", 1)
         
         # 2D Mask and 3D Mask (Browse)
-        self.entry_mask2d = self.create_input_row_with_browse(self.frame_input, "2D Mask (Optional):", 2)
-        self.entry_mask3d = self.create_input_row_with_browse(self.frame_input, "3D Mask (Optional):", 3)
+        self.entry_mask2d     = self.create_input_row_with_browse(self.frame_input, "2D Mask (Optional):", 2)
+        self.entry_mask2dpost = self.create_input_row_with_browse(self.frame_input, "2D Mask Post Smoothing (Optional):", 3)
         
         # Small Entries (Cube Extension and Variance Extension)
         self.entry_extcub = self.create_small_input(self.frame_input, "Cube Ext (Default=0):", 0, "0")  
@@ -67,31 +53,31 @@ class GUI_SHINE:
         self.frame_extraction = tk.LabelFrame(root, text="Extraction Arguments", padx=10, pady=10)
         self.frame_extraction.grid(row=1, column=0, padx=10, pady=5, sticky="ew")
 
-        self.entry_snthresh = self.create_input_row(self.frame_extraction, "S/N Threshold (Default=2):", 0, "2")
-        self.entry_spatsmooth = self.create_input_row(self.frame_extraction, "Spatial Smoothing (Default=0):", 1, "0")
-        self.entry_spatsmoothX = self.create_input_row(self.frame_extraction, "Smooth X (Optional):", 2)
-        self.entry_spatsmoothY = self.create_input_row(self.frame_extraction, "Smooth Y (Optional):", 3)
-        self.entry_specsmooth = self.create_input_row(self.frame_extraction, "Spectral Smoothing (Default=0):", 4, "0")
+        self.entry_snthresh     = self.create_input_row(self.frame_extraction, "S/N Threshold (Default=2):", 0, "2")
+        self.entry_spatsmooth   = self.create_input_row(self.frame_extraction, "Spatial Smoothing (Default=0):", 1, "0")
+        self.entry_spatsmoothX  = self.create_input_row(self.frame_extraction, "Smooth X (Optional):", 2)
+        self.entry_spatsmoothY  = self.create_input_row(self.frame_extraction, "Smooth Y (Optional):", 3)
+        self.entry_specsmooth   = self.create_input_row(self.frame_extraction, "Spectral Smoothing (Default=0, TBD!):", 4, "0")
         self.entry_connectivity = self.create_input_row(self.frame_extraction, "Connectivity (Default=26):", 5, "26")
-        self.entry_maskspatedge = self.create_input_row(self.frame_extraction, "Mask Spatial Edge (Default=20):", 6, "20")
+        self.entry_maskspatedge = self.create_input_row(self.frame_extraction, "Mask Spatial Edges (Default=20):", 6, "20")
 
         # === Cleaning Arguments Section (grpcln) ===
         self.frame_cleaning = tk.LabelFrame(root, text="Cleaning Arguments", padx=10, pady=10)
         self.frame_cleaning.grid(row=2, column=0, padx=10, pady=5, sticky="ew")
 
-        self.entry_minvox = self.create_input_row(self.frame_cleaning, "Min Voxels (Default=1):", 0, "1")
-        self.entry_mindz = self.create_input_row(self.frame_cleaning, "Min DZ (Default=3):", 1, "3")
-        self.entry_maxdz = self.create_input_row(self.frame_cleaning, "Max DZ (Default=200):", 2, "200")
+        self.entry_minvox  = self.create_input_row(self.frame_cleaning, "Min Voxels (Default=1):", 0, "1")
+        self.entry_mindz   = self.create_input_row(self.frame_cleaning, "Min DZ (Default=3):", 1, "3")
+        self.entry_maxdz   = self.create_input_row(self.frame_cleaning, "Max DZ (Default=200):", 2, "200")
         self.entry_minarea = self.create_input_row(self.frame_cleaning, "Min Area (Default=3):", 3, "3")
 
         # === Output Control Arguments Section (grpout) ===
         self.frame_output = tk.LabelFrame(root, text="Output Control Arguments", padx=10, pady=10)
         self.frame_output.grid(row=4, column=0, padx=10, pady=5, sticky="ew")
 
-        self.var_outdir = self.create_input_row_with_browse_dir(self.frame_output, "Output directory (Deafault=./):", 0)
-        self.var_writelabels = tk.BooleanVar()
-        self.var_writesmcube = tk.BooleanVar()
-        self.var_writesmvar = tk.BooleanVar()
+        self.var_outdir         = self.create_input_row_with_browse_dir(self.frame_output, "Output directory (Deafault=./):", 0)
+        self.var_writelabels    = tk.BooleanVar(value=1)
+        self.var_writesmcube    = tk.BooleanVar()
+        self.var_writesmvar     = tk.BooleanVar()
         self.var_writesmsnrcube = tk.BooleanVar()
 
         tk.Checkbutton(self.frame_output, text="Write Labels", variable=self.var_writelabels).grid(row=1, column=0, sticky="w")
@@ -139,9 +125,9 @@ class GUI_SHINE:
     
     def create_small_input(self, parent, label, row, default_value=""):
         """Helper function to create a small input field next to an existing row."""
-        tk.Label(parent, text=label).grid(row=row, column=3, sticky="e", padx=5, pady=2)  # Label nella colonna 3
-        entry = tk.Entry(parent, width=5)  # Entry più piccola
-        entry.grid(row=row, column=4, padx=5, pady=2)  # Entry nella colonna 4
+        tk.Label(parent, text=label).grid(row=row, column=3, sticky="e", padx=5, pady=2)  
+        entry = tk.Entry(parent, width=5)  
+        entry.grid(row=row, column=4, padx=5, pady=2)  
         if default_value:
             entry.insert(0, default_value)
         return entry
@@ -220,33 +206,33 @@ class GUI_SHINE:
 
         """Run the external script with the provided parameters."""
         # Collect input arguments
-        cube = self.entry_cube.get()
-        varcube = self.entry_varcube.get()
-        mask2d = self.entry_mask2d.get()
-        mask3d = self.entry_mask3d.get()
-        extcub = self.entry_extcub.get()
-        extvar = self.entry_extvar.get()
+        cube       = self.entry_cube.get()
+        varcube    = self.entry_varcube.get()
+        mask2d     = self.entry_mask2d.get()
+        mask2dpost = self.entry_mask2dpost.get()
+        extcub     = self.entry_extcub.get()
+        extvar     = self.entry_extvar.get()
 
         # Collect extraction arguments
-        snthresh = self.entry_snthresh.get()
-        spatsmooth = self.entry_spatsmooth.get()
-        spatsmoothX = self.entry_spatsmoothX.get()
-        spatsmoothY = self.entry_spatsmoothY.get()
-        specsmooth = self.entry_specsmooth.get()
+        snthresh     = self.entry_snthresh.get()
+        spatsmooth   = self.entry_spatsmooth.get()
+        spatsmoothX  = self.entry_spatsmoothX.get()
+        spatsmoothY  = self.entry_spatsmoothY.get()
+        specsmooth   = self.entry_specsmooth.get()
         connectivity = self.entry_connectivity.get()
         maskspatedge = self.entry_maskspatedge.get()
 
         # Collect cleaning arguments
-        minvox = self.entry_minvox.get()
-        mindz = self.entry_mindz.get()
-        maxdz = self.entry_maxdz.get()
+        minvox  = self.entry_minvox.get()
+        mindz   = self.entry_mindz.get()
+        maxdz   = self.entry_maxdz.get()
         minarea = self.entry_minarea.get()
 
         # Collect output control arguments
-        outdir = self.var_outdir.get()
-        writelabels = self.var_writelabels.get()
-        writesmcube = self.var_writesmcube.get()
-        writesmvar = self.var_writesmvar.get()
+        outdir         = self.var_outdir.get()
+        writelabels    = self.var_writelabels.get()
+        writesmcube    = self.var_writesmcube.get()
+        writesmvar     = self.var_writesmvar.get()
         writesmsnrcube = self.var_writesmsnrcube.get()
         
 
@@ -257,13 +243,14 @@ class GUI_SHINE:
             f"--extcub={extcub}", f"--extvar={extvar}",
             f"--snthresh={snthresh}", f"--spatsmooth={spatsmooth}",
             f"--connectivity={connectivity}", f"--maskspedge={maskspatedge}",
+            f"--minvox={minvox}", f"--mindz={mindz}", f"--maxdz={maxdz}", f"--minarea={minarea}",
         ]
 
         # Add optional arguments
         if mask2d:
             command.append(f"--mask2d={mask2d}")
-        if mask3d:
-            command.append(f"--mask3d={mask3d}")
+        if mask2dpost:
+            command.append(f"--mask2dpost={mask2dpost}")
         if spatsmoothX:
             command.append(f"--spatsmoothX={spatsmoothX}")
         if spatsmoothY:
