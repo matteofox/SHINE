4580b7b7b83d6eaec791edb509f0532c8c4d8520
matteofox <matteofox88@gmail.com>
2024-12-12 19:47:40 +0100

    Updated docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -27,8 +27,6 @@ extensions = [
 ]
 
 
-
-
 templates_path = ['_templates']
 exclude_patterns = []
 
@@ -42,12 +40,9 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+    html_theme = "sphinx_rtd_theme"
 
 
-html_theme = "sphinx_rtd_theme"
-
-
-#html_theme = 'alabaster'
 #html_static_path = ['_static']
 
 # Output file base name for HTML help builder.
