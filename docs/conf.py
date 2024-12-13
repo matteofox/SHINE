3f8ade7ea3e1e54a62be057ba4d9b93713e3bb66
matteofox <matteofox88@gmail.com>
2024-12-13 08:19:23 +0100

    Updated docs conf

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
     'sphinx.ext.napoleon',
+    'sphinx_rtd_theme'
 ]
 
 
@@ -40,7 +41,8 @@ on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 if not on_rtd:  # only import and set the theme if we're building docs locally
     import sphinx_rtd_theme
     html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-    html_theme = "sphinx_rtd_theme"
+
+html_theme = "sphinx_rtd_theme"
 
 
 #html_static_path = ['_static']
